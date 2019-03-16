6c4c1f90d40e2cc77fced048e12e4d732d3e26e6
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-03-16 21:12:44 +0100

    script refactor

--- SCRIPTS/selectedCountry.js
+++ SCRIPTS/selectedCountry.js
@@ -1,7 +1,15 @@
 const btnSearch = document.querySelector('button');
+// const city
 let selectedCountry = null;
 
 btnSearch.addEventListener('click', () => {
     selectedCountry = document.getElementById('myInput').value;
-    console.log(selectedCountry);
+    checkingCountry(selectedCountry);
 });
+
+const checkingCountry = (country) => {
+    console.log(country);
+    if (country == "France" || country == "Germany" || country == "Poland" || country == "Spain") {} else {
+        alert("Unavailable country. Select France, Germany, Poland or Spain")
+    }
+}
