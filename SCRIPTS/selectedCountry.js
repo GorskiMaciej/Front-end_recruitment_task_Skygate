2aafb44aa058116502d358b9c316240eb55d2525
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-03-26 23:01:01 +0100

    resolving problem with async - adding awaits

--- SCRIPTS/selectedCountry.js
+++ SCRIPTS/selectedCountry.js
@@ -3,9 +3,10 @@ const cityContainer = document.querySelector('.cities-container');
 let selectedCountry = null;
 let countryCode = "";
 let typeOfPollution = "pm25";
-let numberOfMostPollutedCieties = 10;
+let numberOfMostPollutedCieties = 79;
 let mostPollutedCities = [];
-let arrayAll = [2];
+let arrayMeasurementValues = [];
+let arrayCitiesNames = [];
 let url = `https://api.openaq.org/v1/latest?country=${countryCode}&parameter=${typeOfPollution}`;
 let urlCountries = `https://api.openaq.org/v1/countries`;
 
@@ -17,7 +18,7 @@ const turnOnCityContainer = (element) => {
 };
 
 //getting country code from json depending on the selected country
-const gettingCountryCode = () => {
+const gettingCountryCode = (callback) => {
     fetch(urlCountries)
         .then(res => res.json())
         .then((out) => {
@@ -32,7 +33,7 @@ const gettingCountryCode = () => {
         .catch(err => {
             throw err
         });
-    return countryCode + "";
+    callback();
 };
 
 
@@ -49,40 +50,75 @@ const addingMostPollutedCitiesToArray = (urlParameter) => {
         });
 };
 
-// const addingMostPollutedCitiesToArray = (x, countryCodeParameter) => {
-//     $.getJSON(`https://api.openaq.org/v1/latest?country=${countryCodeParameter}&parameter=${typeOfPollution}`, (data) => {
-//         console.log(data); //debug
-//         let arrayAll = [];
-//         for (let i = 0; i < data.results.length; i++) {
-//             arrayAll[i] = data.results[i].measurements[0].value * 1;
-//         }
-//         arrayAll.sort(function (a, b) {
-//             return b - a
-//         });
-//         arrayAll = arrayAll.splice(0, numberOfMostPollutedCieties);
-
-//         for (let i = 0; i < numberOfMostPollutedCieties; i++) {
-//             for (let j = 0; j < 77; j++) {
-//                 if (arrayAll[i] == data.results[j].value) {
-//                     mostPollutedCities[i] = data.results[j].name;
-//                     console.log("mostPollutedCities: " + mostPollutedCities); //debug
-//                 }
-//             }
-//         }
-//         console.log("mostPollutedCities: " + mostPollutedCities); //debug
-//     });
-
-// }
-
-btnSearch.addEventListener('click', () => {
+async function getMostPollutedCities() {
     selectedCountry = document.getElementById('myInput').value;
     if (selectedCountry == "France" || selectedCountry == "Germany" || selectedCountry == "Poland" || selectedCountry == "Spain") {
         turnOnCityContainer(cityContainer);
-        countryCode = gettingCountryCode();
-        console.log("country code: " + countryCode); // country code is not updated
-        addingMostPollutedCitiesToArray(url); //url is not updated after click
+
+        //fetching countries and getting countryCode
+        await fetch(urlCountries)
+            .then(res => res.json())
+            .then((code) => {
+                for (let i = 0; i < code.results.length; i++) {
+                    if (code.results[i].name == selectedCountry) {
+                        countryCode = code.results[i].code;
+                    }
+                }
+                console.log(countryCode); // debug to show code of selected country
+            })
+            .catch(err => {
+                throw err
+            });
+
+        url = `https://api.openaq.org/v1/latest?country=${countryCode}&parameter=${typeOfPollution}`
+        await console.log(url); // debug to check if url is correct
+
+        await fetch(url)
+            .then(res => res.json())
+            .then((out) => {
+                console.log(out); //debug
+                for (let i = 0; i < out.results.length; i++) {
+                    arrayMeasurementValues[i] = out.results[i].measurements[0].value;
+                    arrayCitiesNames[i] = out.results[i].city;
+
+                }
+                console.log("ArrayValues after init: " + arrayMeasurementValues);
+                console.log("ArrayCities after init: " + arrayCitiesNames);
+
+                arrayMeasurementValues.sort(function (a, b) {
+                    return b - a
+                });
+
+                console.log("ArrayValues after sorting: " + arrayMeasurementValues);
+
+                // arrayMeasurementValues = arrayMeasurementValues.splice(0, numberOfMostPollutedCieties);
+
+                // console.log("ArrayValues after splice: " + arrayMeasurementValues);
+                let pairCityValue = [];
+                for (let i = 0; i < numberOfMostPollutedCieties; i++) {
+                    for (let j = 0; j < out.results.length; j++) {
+                        if (arrayMeasurementValues[i] === out.results[j].measurements[0].value) {
+
+
+                            // console.log(arrayMeasurementValues[i]);
+                            // console.log(out.results[j].measurements[0].value + " - " + out.results[j].city);
+                            let pair = new Object();
+                            pair.city = out.results[j].city;
+                            pair.value = out.results[j].measurements[0].value;
+                            pairCityValue[i] = pair;
+                            mostPollutedCities[i] = out.results[j].city;
+                        }
+                    }
+                }
+                console.log("mostPollutedCities: " + mostPollutedCities); //debug
+                console.log("mostPollutedCities-pairs: " + pairCityValue[0]); //debug
+            })
+
+
     } else {
         alert("Unavailable country. Select France, Germany, Poland or Spain")
     }
 
-});
+}
+
+btnSearch.addEventListener('click', getMostPollutedCities);
